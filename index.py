215bd5fdcfc086605f98305de04d328379583307
Irony00 <r.rahul.developer@gmail.com>
2021-11-02 01:29:36 +0000

    Removed __pycache__

--- index.py
+++ index.py
@@ -4,11 +4,5 @@ from detector import Detector
 detector = Detector("Eigen")
 
 detector.train_model()
+detector.save_model()
 
-# test_image = cv2.imread("Testing/2.jpg")
-# cropped_face = detector.crop_face(test_image)
-
-# if not len(cropped_face):
-#     print("No face detected")
-# else:
-#     cv2.imwrite("output.jpg", cropped_face)
